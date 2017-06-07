8a3a036a10811f53b0ccd0d0e6b8213588a0a4c7
CheolminConanShin <scm0226@gmail.com>
2017-06-07 16:02:32 +0900

    GOAL: Negative testing

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var should = require('should');
 
 describe('Sample', () => {
     it('should return hello', () => {
-        "hello2".should.equal("hello")
+        "hello".should.equal("hello")
     })
 })
